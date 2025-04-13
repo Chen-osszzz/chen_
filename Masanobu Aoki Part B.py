ec281f0552a54dc31b9a1b8c573273d20ea7a6bb
MasanobuAoki <tur76989@temple.edu>
2025-04-13 13:22:52 +0900

    Update Masanobu Aoki Part B.py

--- Masanobu Aoki Part B.py
+++ Masanobu Aoki Part B.py
@@ -1,4 +1,4 @@
-#Take the text file day1 through day7 and store them into a dictionary
+#Take the text file day1 through day7 and store them into a dictionary with the corresponding key
 def load_log_from_file(filename_day1, filename_day2, filename_day3, filename_day4, filename_day5, filename_day6, filename_day7):
   #Create keys and empty lists that goes with the key
   log_data = {
@@ -30,8 +30,7 @@ def load_log_from_file(filename_day1, filename_day2, filename_day3, filename_day
           elif line[:18] == "Exercise duration:":
               log_data["Exercise duration"].append(line[18:])
   return log_data
-
-
+#Calculate for calories statisitics
 def calories_stat(log_data):
   count_days=len(log_data["Day"])
   calories_num_list=[]
@@ -43,7 +42,7 @@ def calories_stat(log_data):
       total_calories+=j
   avg_calories=total_calories/count_days
   return total_calories, avg_calories
-
+#Calculate for sleep statistics
 def sleep_stat(log_data):
       count_days=len(log_data["Day"])
       sleep_num_list=[]
@@ -55,7 +54,7 @@ def sleep_stat(log_data):
           total_sleep+=k
       avg_sleep=total_sleep/count_days
       return total_sleep, avg_sleep
-
+#Calculate water intake statistics
 def water_stat(log_data):
   count_days=len(log_data["Day"])
   water_num_list=[]
@@ -67,7 +66,7 @@ def water_stat(log_data):
       total_water+=i
   avg_water=total_water/count_days
   return total_water, avg_water
-
+#Calculate duration of the exercise statistics
 def exercise_duration_stat(log_data):
   count_days=len(log_data["Day"])
   exercise_duration_num_list=[]
@@ -80,6 +79,7 @@ def exercise_duration_stat(log_data):
   avg_exercise_duration=total_exercise_duration/count_days
   return total_exercise_duration, avg_exercise_duration
 
+#Calling all the function above and returning all calculated values
 def weekly_summary(log_data):
   total_calories, avg_calories=calories_stat(log_data)
   total_sleep, avg_sleep=sleep_stat(log_data)
@@ -87,5 +87,5 @@ def weekly_summary(log_data):
   total_exercise_duration, avg_exercise_duration=exercise_duration_stat(log_data)
   return total_calories, avg_calories, total_sleep, avg_sleep, total_water, avg_water, total_exercise_duration, avg_exercise_duration
     
-   
-print(weekly_summary(load_log_from_file("day1.txt", "day2.txt", "day3.txt", "day4.txt", "day5.txt", "day6.txt", "day7.txt")))
+#The code below is a suggestion for calling all the functions above nd return all the statistics 
+#print(weekly_summary(load_log_from_file("day1.txt", "day2.txt", "day3.txt", "day4.txt", "day5.txt", "day6.txt", "day7.txt")))
